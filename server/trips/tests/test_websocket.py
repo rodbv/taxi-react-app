941e8fc36e2b8b936824953821d73005aacf51a6
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2020-10-12 17:16:57 -0300

    Add driver to group

--- server/trips/tests/test_websocket.py
+++ server/trips/tests/test_websocket.py
@@ -7,6 +7,7 @@ from channels.db import database_sync_to_async
 from taxi.routing import application
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.tokens import AccessToken
+from django.contrib.auth.models import Group
 
 
 TEST_CHANNEL_LAYERS = {
@@ -17,12 +18,25 @@ TEST_CHANNEL_LAYERS = {
 
 
 @database_sync_to_async
-def create_user(username, password):
+def create_user(  # changed
+        username,
+        password,
+        group='rider'
+):
+    # Create user.
     user = get_user_model().objects.create_user(
         username=username,
         password=password
     )
+
+    # Create user group.
+    user_group, _ = Group.objects.get_or_create(name=group)  # new
+    user.groups.add(user_group)
+    user.save()
+
+    # Create access token.
     access = AccessToken.for_user(user)
+
     return user, access
 
 
@@ -61,10 +75,10 @@ class TestWebSocket:
         assert response == message
         await communicator.disconnect()
 
-    async def test_can_send_and_receive_broadcast_messages(self, settings):
+    async def test_join_driver_pool(self, settings):
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
         _, access = await create_user(
-            'test.user@example.com', 'pAssw0rd'
+            'test.user@example.com', 'pAssw0rd', 'driver'
         )
         communicator = WebsocketCommunicator(
             application=application,
@@ -76,7 +90,7 @@ class TestWebSocket:
             'data': 'This is a test message.',
         }
         channel_layer = get_channel_layer()
-        await channel_layer.group_send('test', message=message)
+        await channel_layer.group_send('drivers', message=message)
         response = await communicator.receive_json_from()
         assert response == message
         await communicator.disconnect()

94bfa5d922726b19391a1191cd70f8fe91d2526b
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2020-10-14 22:25:35 -0300

    Login form

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,26 +1,64 @@
-
 import React from 'react';
-import { Breadcrumb, Card, Col, Row } from 'react-bootstrap'; // new
+import { Formik } from 'formik';
+import { Breadcrumb, Button, Card, Col, Form, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-// changed
-function LogIn (props) {
+function LogIn(props) {
   return (
-    <Row>
-      <Col lg={12}>
-        <Breadcrumb>
-          <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
-          <Breadcrumb.Item active>Log in</Breadcrumb.Item>
-        </Breadcrumb>
-        <Card>
-          <Card.Header>Log in</Card.Header>
-          <Card.Body></Card.Body>
-          <p className='mt-3 text-center'>
-            Don't have an account? <Link to='/sign-up'>Sign up!</Link>
-          </p>
-        </Card>
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col lg={12}>
+          <Breadcrumb>
+            <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+            <Breadcrumb.Item active>Log in</Breadcrumb.Item>
+          </Breadcrumb>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col lg={6}>
+          <Card>
+            <Card.Header>Log in</Card.Header>
+            <Card.Body>
+              <Formik
+                initialValues={{
+                  username: '',
+                  password: '',
+                }}
+                onSubmit={console.log}
+              >
+                {({ handleChange, handleSubmit, values }) => (
+                  <Form noValidate onSubmit={handleSubmit}>
+                    <Form.Group controlId="username">
+                      <Form.Label>Username:</Form.Label>
+                      <Form.Control
+                        name="username"
+                        onChange={handleChange}
+                        value={values.username}
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="password">
+                      <Form.Label>Password:</Form.Label>
+                      <Form.Control
+                        name="password"
+                        onChange={handleChange}
+                        type="password"
+                        value={values.password}
+                      />
+                    </Form.Group>
+                    <Button block type="submit" variant="primary">
+                      Log in
+                    </Button>
+                  </Form>
+                )}
+              </Formik>
+            </Card.Body>
+            <p className="mt-3 text-center">
+              Don't have an account? <Link to="/sign-up">Sign up!</Link>
+            </p>
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 }
 

3e9ba22981d563621d40f4dcf78dd3b23bca6951
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2020-10-14 22:12:48 -0300

    React bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom'; // changed
+import { Container, Navbar } from 'react-bootstrap'; // new
+import { LinkContainer } from 'react-router-bootstrap'; // new
+import { Link, Route, Switch } from 'react-router-dom';
 
-import SignUp from './components/SignUp'; // new
-import LogIn from './components/LogIn'; // new
+import SignUp from './components/SignUp';
+import LogIn from './components/LogIn';
 
 import './App.css';
 
 // changed
 function App () {
   return (
-    <Switch>
-      <Route exact path='/' render={() => (
-        <div>
-          <h1>Taxi</h1>
-          <Link to='/sign-up'>Sign up</Link>
-          <Link to='/log-in'>Log in</Link>
-        </div>
-      )} />
-      <Route path='/sign-up' component={SignUp} />
-      <Route path='/log-in' component={LogIn} />
-    </Switch>
+    <>
+      <Navbar bg='light' expand='lg' variant='light'>
+        <LinkContainer to='/'>
+          <Navbar.Brand className='logo'>Taxi</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle />
+        <Navbar.Collapse></Navbar.Collapse>
+      </Navbar>
+      <Container className='pt-3'>
+        <Switch>
+          <Route exact path='/' render={() => (
+            <div className='middle-center'>
+              <h1 className='landing logo'>Taxi</h1>
+              <Link className='btn btn-primary' to='/sign-up'>Sign up</Link>
+              <Link className='btn btn-primary' to='/log-in'>Log in</Link>
+            </div>
+          )} />
+          <Route path='/sign-up' component={SignUp} />
+          <Route path='/log-in' component={LogIn} />
+        </Switch>
+      </Container>
+    </>
   );
 }
 
